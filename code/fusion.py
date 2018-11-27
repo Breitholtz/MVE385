b94d0a141c073c5927ec9aeaf9b0dc494e2765d6
Breitholtz <cadbreitholtz@gmail.com>
2018-11-27 11:02:07 +0100

    data and code folders

---
+++ code/fusion.py
@@ -0,0 +1,28 @@
+#
+# This is the shell code for the image fusion program
+# 
+
+import ReadBIF6 as read
+import cache
+# Read in Bif6-file
+
+
+# PCA to find most relevant data to fuse
+
+
+# (processing to deal with skewed images etc. if needed!)
+
+
+# Take data and use chosen algorithm to fuse the images
+
+
+# return final image with correlation data to the pre-fusion images
+
+
+
+def main(): #(image1,image2):
+	print("Hello")
+		
+
+if  __name__ =='__main__':
+	main()
