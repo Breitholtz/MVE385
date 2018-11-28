b883c92055913fb01e7e1f88d64c2e1a42b29be8
Emil Carlsson <caremil@student.chalmers.se>
2018-11-28 10:47:52 +0100

    Update wavelet_fusion.py

--- code/wavelet_fusion.py
+++ code/wavelet_fusion.py
@@ -3,15 +3,21 @@ import numpy as np
 import pywt
 import pywt.data
 
-def wavelet_image_fusion(hyper_image_1, hyper_image_2, region_to_fuse):
+def wavelet_image_fusion(hyper_image_1, hyper_image_2, region_to_fuse,depth_of_wavelet):
  #Image 1 and 2 must have be 3-dimensional arrays with sizes n*m*k
  #region_to_fuse is a n*m boolean array telling which regions to fuse in the images
+ #depth_of_wavelet, the nbr of levels in the multilevel 2D wavelet transform
  tmp1,tmp2,nbr_spectrs = size(hyper_image_1)
   # Fuse each spectrum layer
   for i in (0,nbr_spectrs):
    # perform (multilevel) 2D wavelet transform
    # To do: implement multilevel wavelet and handle arbitrary outputs
-   approx_coeffs_1, (horizontial_coeffs_1, vertixal_coeffs_1, diagonal_coeffs_1) = pywt.wavedec2(hyper_image_1, gaus, level=1)
-   approx_coeffs_1, (horizontial_coeffs_1, vertixal_coeffs_1, diagonal_coeffs_1) = pywt.wavedec2(hyper_image_1, gaus, level=1)
+   #
+   approx_coeffs_1, (horizontial_coeffs_1, vertixal_coeffs_1, diagonal_coeffs_1) = pywt.wavedec2(hyper_image_1, haar, level=1)
+   approx_coeffs_2, (horizontial_coeffs_2, vertixal_coeffs_2, diagonal_coeffs_2) = pywt.wavedec2(hyper_image_2, haar, level=1)
+   
+   for j in (0,depth_of_wavelet):
+    
+   
   
   
