6dbddde7bc5dd720b518486e6bf5b66f499a2991
Breitholtz <cadbreitholtz@gmail.com>
2018-12-05 13:31:59 +0100

    updated

--- code/imcontrast.m
+++ code/imcontrast.m
@@ -5,14 +5,15 @@ function f=imcontrast(I)
 N=size(I,1);
 M=size(I,2);
 
-Ibar=mean(I);
+Ibar=mean2(I);
 
 sum=0;
 for i=1:N
 	for j=1:M
-		sum+=(I(i,j)/Ibar-1)^2;
+		sum=sum+(I(i,j)/Ibar-1)^2;
 	end
 end
+disp(sum)
 result=sqrt(sum);
 
 disp(result);
--- code/imquality.m
+++ code/imquality.m
@@ -1,20 +1,24 @@
 function f=imquality(I,I2)
 %
-% image quality function
+% image quality function; takes two images of identical size atm.
 %
-for k=1:3
+N=size(I,1);
+M=size(I,2);
+K=size(I,3);
+
+for k=1:K
 F=fft(I(:,:,k));
 Fcenter=fftshift(F);
 F2=fft(I2(:,:,k));
 Fcenter2=fftshift(F2);
 
-M=max(max(abs(Fcenter)));
+M1=max(max(abs(Fcenter)));
 M2=max(max(abs(Fcenter2)));
 T_h1=0;
 T_h2=0;
-for i=1:size(I(:,1,1))
-    for j=1:size(I(1,:,1))
-        if (abs(Fcenter(i,j))>M/1000)
+for i=1:N
+    for j=1:M
+        if (abs(Fcenter(i,j))>M1/1000)
             T_h1=T_h1+1;
         end
         if (abs(Fcenter2(i,j))>M2/1000)
@@ -24,9 +28,9 @@ for i=1:size(I(:,1,1))
 end
 
 disp('high res: layer'+ k);
-high(k)=T_h1/(size(I(:,1,1),1).*size(I(1,:,1),2));
+high(k)=T_h1/(N*M);
 disp('low res: layer'+k);
-low(k)=T_h2/(size(I(:,1,1),1).*size(I(1,:,1),2));
+low(k)=T_h2/(N*M);
 end
 mean(high)
 mean(low)
